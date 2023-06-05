235b91c0ef79bb8f7ac9fa2b74f4c32dfa111f0d
Lindsay Ward <lward23@hotmail.com>
2023-06-05 15:26:01 -0700

    Update polls table schema and seeds

--- db/schema/02_polls.sql
+++ db/schema/02_polls.sql
@@ -4,5 +4,8 @@ CREATE TABLE polls (
   id SERIAL PRIMARY KEY NOT NULL,
   title TEXT NOT NULL,
   description TEXT,
+  choice_1 TEXT,
+  choice_2 TEXT,
+  choice_3 TEXT,
   user_id INTEGER REFERENCES users(id)
 );
--- db/seeds/02_polls.sql
+++ db/seeds/02_polls.sql
@@ -1,4 +1,4 @@
-INSERT INTO polls (title, description, user_id) VALUES ('What pizza should we get tonight?', 'If you do not like pineapple on pizza you are not Canadian enough', 3);
-INSERT INTO polls (title, description, user_id) VALUES ('What movie should we watch tonight?', 'Maybe this time we can spend an hour watching a movie instead of picking one?', 1);
-INSERT INTO polls (title, user_id) VALUES ('Which beach should we go to?', 2);
+INSERT INTO polls (title, description, choice_1, choice_2, choice_3, user_id) VALUES ('What pizza should we get tonight?', 'If you do not like pineapple on pizza you are not Canadian enough', 'Pepperoni', 'Hawaiian', 'Cheese', 1);
+INSERT INTO polls (title, description, choice_1, choice_2, choice_3, user_id) VALUES ('What movie should we watch tonight?', 'Maybe this time we can spend an hour watching a movie instead of picking one?', 'The Godfather', 'Legally Blonde', 'Jurassic Park', 2);
+INSERT INTO polls (title, choice_1, choice_2, choice_3, user_id) VALUES ('Which beach should we go to?', 'Kitsilano Beach', 'Jericho Beach', 'English Bay Beach', 3);
 
