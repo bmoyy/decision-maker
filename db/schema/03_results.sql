cb797152e6c3086af37d6d4e48624bb0dad1897a
Lindsay Ward <lward23@hotmail.com>
2023-06-05 15:30:00 -0700

    Rename results and update table schema and seeds

--- db/schema/03_results.sql
+++ db/schema/03_results.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS results CASCADE;
 
 CREATE TABLE results (
   id SERIAL PRIMARY KEY NOT NULL,
-  value TEXT NOT NULL,
+  borda_value_1 INTEGER NOT NULL,
+  borda_value_2 INTEGER NOT NULL,
+  borda_value_3 INTEGER NOT NULL,
   poll_id INTEGER REFERENCES polls(id),
-  user_id INTEGER REFERENCES users(id)
 );
---
+++ db/seeds/03_results.sql
@@ -0,0 +1,13 @@
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (3, 2, 1, 1);
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (2, 3, 1, 1);
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (1, 2, 3, 1);
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (1, 3, 2, 1);
+
+
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (2, 3, 1, 2);
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (2, 1, 3, 2);
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (3, 1, 2, 2);
+
+
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (1, 2, 3, 3);
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (2, 3, 1, 3);
