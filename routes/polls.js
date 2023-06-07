3bb8e1d7acbf9675289c153552591fe98fc4a842
Lindsay Ward <lward23@hotmail.com>
2023-06-07 13:35:23 -0700

    Add getPoll to have info in pollObj for charting results

--- routes/polls.js
+++ routes/polls.js
@@ -1,10 +1,10 @@
 "use strict";
 
 //mailgun
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+// const formData = require('form-data');
+// const Mailgun = require('mailgun.js');
+// const mailgun = new Mailgun(formData);
+// const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
 
 
 const express = require('express');
@@ -65,18 +65,18 @@ router.post('/', (req, res) => {
     .then((poll) => {
 
       //sends email to creator
-      mg.messages.create('sandboxaec54a8b2ab745aba97cc44960e92776.mailgun.org', {
-        from: "Decision Maker <mailgun@sandboxaec54a8b2ab745aba97cc44960e92776.mailgun.org>",
-        to: [`${user.email}`],
-        subject: "Thank you for using Decision Maker!",
-        text: "Provided below are your links to vote and view poll results!",
-        html: `<p>Provided below are your links to view poll results and vote!</p>
-        <a href="http://localhost:8080/polls/${poll[0].id}">Vote on Poll</a>
-        <br>
-        <a href="http://localhost:8080/polls/${poll[0].id}/result">Poll results</a>`
-      })
-      .then(msg => console.log(msg))
-      .catch(err => console.log(err));
+      // mg.messages.create('sandboxaec54a8b2ab745aba97cc44960e92776.mailgun.org', {
+      //   from: "Decision Maker <mailgun@sandboxaec54a8b2ab745aba97cc44960e92776.mailgun.org>",
+      //   to: [`${user.email}`],
+      //   subject: "Thank you for using Decision Maker!",
+      //   text: "Provided below are your links to vote and view poll results!",
+      //   html: `<p>Provided below are your links to view poll results and vote!</p>
+      //   <a href="http://localhost:8080/polls/${poll[0].id}">Vote on Poll</a>
+      //   <br>
+      //   <a href="http://localhost:8080/polls/${poll[0].id}/result">Poll results</a>`
+      // })
+      // .then(msg => console.log(msg))
+      // .catch(err => console.log(err));
 
       return res.render('link', { poll });
     })
@@ -132,14 +132,22 @@ router.post('/:id/vote', (req, res) => {
 
 router.get('/:id/result', (req, res) => {
   const id = req.params.id;
-
-  getTotalRanking(id)
-  .then((result) => {
-    return res.render('result', { result });
+  getPoll(id)
+  .then((data) => {
+    const pollObj = data[0];
+    getTotalRanking(id)
+    .then((result) => {
+      return res.render('result', { result, pollObj });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   })
   .catch((err) => {
     console.log(err);
   });
+
+
 })
 
 module.exports = router;
