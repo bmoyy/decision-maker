76b35588457c350a8b28a728c451d72116ecfc16
Lindsay Ward <lward23@hotmail.com>
2023-06-08 20:36:38 -0700

    Update database poll 3 to create tie result

--- db/seeds/03_results.sql
+++ db/seeds/03_results.sql
@@ -11,3 +11,4 @@ INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUE
 
 INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (1, 2, 3, 3);
 INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (2, 3, 1, 3);
+INSERT INTO results (borda_value_1, borda_value_2, borda_value_3, poll_id) VALUES (3, 2, 3, 3);
